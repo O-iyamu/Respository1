bbb7d1762c29921a404fb110324839e91b462f32
Osas <osasiyamu@cmail.carleton.ca>
2021-09-23 22:45:28 -0400

    This is another commit message.

---
+++ Labs/Lab1/src/BuddyInfo.java
@@ -0,0 +1,26 @@
+public class BuddyInfo {
+
+    private String name = "Ben";
+    private String address ;
+    private String phoneNumber;
+
+    public BuddyInfo() {
+        this("Ben", "252 Emerald St", "+1 (234)7657759");
+    }
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static void main(String[] args) {
+        BuddyInfo homer = new BuddyInfo("Homer", "101 Street", "+1 (227)2342357");
+        System.out.println("Hello " + homer.getName());
+    }
+}
+
